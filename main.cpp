54da5605f2adf287c0ed051d782b14e9e16fd348
Vinay Shankar Dubey <59216953+vinayshankardubey@users.noreply.github.com>
2021-09-10 04:50:22 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+
+//Days2
+//Today I have learn about C++ Input/Output And If/Else Statements.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+ cout << "Hello Welcome To this Day2 Challenge!\n";
+
+ //Input a number from User
+ int marks;
+ cout << "Hey! User Please write your Total marks Then I can talk you to You are Passed or Fail"<<endl;
+ cin >> marks;
+
+ if(marks >= 500){
+
+   cout << "Hurrah! You are Passed"<<endl;
+
+ }else{
+
+   cout << "Ohh God ....You Are Failed!"<<endl;
+
+ }
+
+}
